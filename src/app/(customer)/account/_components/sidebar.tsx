fad366c8a5cc7b0bd42df347ec49c5e8eaed10d5
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2025-09-29 23:29:02 +0700

    feat(ui): add profile page layout and components

--- src/app/(customer)/account/_components/sidebar.tsx
+++ src/app/(customer)/account/_components/sidebar.tsx
@@ -1,14 +1,43 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { SidebarLink } from "./sidebar-link";
+import { usePathname } from "next/navigation";
+
+const sidebarLink: { label: string; path: string }[] = [
+  {
+    label: "Informasi Pribadi",
+    path: "/account/profile",
+  },
+  {
+    label: "Pesanan Saya",
+    path: "/account/orders",
+  },
+  {
+    label: "Metode Pembayaran",
+    path: "/account/payments",
+  },
+  {
+    label: "Alamat",
+    path: "/account/address",
+  },
+];
 
 export const Sidebar = () => {
+  const pathName = usePathname();
   return (
     <nav className="h-5/6 w-5/6 flex flex-col justify-between">
       <ul className="flex flex-col gap-4 ">
-        <SidebarLink href="/profile">Informasi Pribadi</SidebarLink>
-        <SidebarLink href="/orders">Pesanan Saya</SidebarLink>
-        <SidebarLink href="/payments">Metode Pembayaran</SidebarLink>
-        <SidebarLink href="/address">Alamat</SidebarLink>
+        {sidebarLink.map((link) => {
+          return (
+            <SidebarLink
+              key={link.path}
+              isActive={pathName === link.path}
+              href={link.path}
+            >
+              {link.label}
+            </SidebarLink>
+          );
+        })}
       </ul>
 
       <div>
--- src/app/(customer)/account/layout.tsx
+++ src/app/(customer)/account/layout.tsx
@@ -1,14 +1,25 @@
+"use client";
+import { usePathname } from "next/navigation";
 import { Sidebar } from "./_components/sidebar";
 
 export default function AccountLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const pathName = usePathname();
+  console.log(pathName);
+
   return (
     <div className="h-full w-full flex flex-row py-10">
       <div className="h-full w-2/6 pr-4 bg-background flex flex-col items-end">
         <Sidebar />
       </div>
-      <div className="h-full w-4/6 rounded-lg border-foreground border-2">
+      <div
+        className={`h-full w-4/6 rounded-lg ${
+          pathName === "/account/profile"
+            ? "border-transparent"
+            : "border-foreground"
+        } border-2`}
+      >
         {children}
       </div>
     </div>
