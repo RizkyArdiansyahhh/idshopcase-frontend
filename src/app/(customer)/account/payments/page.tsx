bef4c1ae96fd96ca1ac19e446d81205b0ac23a6a
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2025-10-04 14:05:31 +0700

    feat(ui): add field and radio-group component

---
+++ src/app/(customer)/account/payments/page.tsx
@@ -0,0 +1,11 @@
+import { ChoiceCard } from "@/components/shared/choice-card";
+
+const PaymentsPage = () => {
+  return (
+    <>
+      <ChoiceCard></ChoiceCard>
+    </>
+  );
+};
+
+export default PaymentsPage;
---
+++ src/components/shared/choice-card.tsx
@@ -0,0 +1,3 @@
+export const ChoiceCard = () => {
+  return <div>ChoiceCard</div>;
+};
