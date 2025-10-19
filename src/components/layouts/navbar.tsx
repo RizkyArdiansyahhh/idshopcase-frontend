586440411aee17fc7b86bf0e8143b69982614398
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2025-10-19 14:02:15 +0700

    chore(react-query): setup QueryClientProvider in layout

--- src/components/layouts/navbar.tsx
+++ src/components/layouts/navbar.tsx
@@ -57,9 +57,14 @@ export const Navbar = ({ isBlur = false }: NavbarProps) => {
           <Link href={"/account/profile"}>
             <FaUser size={24} color="background" />
           </Link>
-          <Link href={"/cart"}>
-            <FaCartArrowDown size={24} color="background" />
-          </Link>
+          <div className="relative">
+            <Link href={"/cart"}>
+              <FaCartArrowDown size={24} color="background" />
+            </Link>
+            <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">
+              3
+            </span>
+          </div>
         </div>
       </div>
     </nav>
