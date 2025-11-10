e0e03f0d09bf422b7feb90b0050916ebd507706c
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2025-11-11 04:13:53 +0700

    refactor orders customer

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -33,6 +33,21 @@ function TabsList({
     />
   );
 }
+function TabsListV2({
+  className,
+  ...props
+}: React.ComponentProps<typeof TabsPrimitive.List>) {
+  return (
+    <TabsPrimitive.List
+      data-slot="tabs-list"
+      className={cn(
+        "bg-muted text-muted-foreground inline-flex h-9 w-fit items-center justify-evenly rounded-lg p-[3px]",
+        className
+      )}
+      {...props}
+    />
+  );
+}
 
 function TabsTrigger({
   className,
@@ -43,6 +58,40 @@ function TabsTrigger({
       data-slot="tabs-trigger"
       className={cn(
         "data-[state=active]:bg-background dark:data-[state=active]:text-foreground focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring dark:data-[state=active]:border-input dark:data-[state=active]:bg-input/30 text-foreground dark:text-muted-foreground inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 rounded-md border border-transparent px-6 py-1 text-sm font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+function TabsTriggerV2({
+  className,
+  ...props
+}: React.ComponentProps<typeof TabsPrimitive.Trigger>) {
+  return (
+    <TabsPrimitive.Trigger
+      data-slot="tabs-trigger"
+      className={cn(
+        // Base layout
+        "inline-flex flex-1  items-center justify-center gap-1.5 rounded-full border px-10 py-3 text-sm font-medium whitespace-nowrap transition-[color,box-shadow]",
+
+        // Text colors
+        "text-foreground dark:text-muted-foreground",
+
+        // Disabled state
+        "disabled:pointer-events-none disabled:opacity-50",
+
+        // Focus ring
+        "focus-visible:outline-none focus-visible:ring-[3px] focus-visible:ring-ring/50 focus-visible:outline-ring",
+
+        // Active state
+        "data-[state=active]:bg-foreground data-[state=active]:shadow-sm data-[state=active]:text-background",
+        "dark:data-[state=active]:bg-input/30 dark:data-[state=active]:border-input dark:data-[state=active]:text-background",
+
+        // SVG adjustments
+        "[&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+
+        // Additional (custom) classes
         className
       )}
       {...props}
@@ -63,4 +112,4 @@ function TabsContent({
   );
 }
 
-export { Tabs, TabsList, TabsTrigger, TabsContent };
+export { Tabs, TabsList, TabsTrigger, TabsContent, TabsListV2, TabsTriggerV2 };
